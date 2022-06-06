4e2463ce5fef50e4aacb21ab3f90cf6ad44a009f
Wtr <tahjudil.witra@gmail.com>
2022-06-06 11:32:22 +0700

    initialize test functions

---
+++ tests/tests_raster/processing.py
@@ -0,0 +1,14 @@
+from simplerqgis.raster.processing import *
+from simplerqgis.initializations import set_qgsapplication_instance
+
+
+def test_reproject_raster():
+    pass
+
+
+def test_clip_raster_by_mask():
+    pass
+
+
+def test_clip_raster_by_feature():
+    pass
