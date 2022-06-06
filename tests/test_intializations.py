132c18af550b8c300e6d7977c770e9b569971c42
Wtr <tahjudil.witra@gmail.com>
2022-06-06 10:36:24 +0700

    add a test

--- tests/test_intializations.py
+++ tests/test_intializations.py
@@ -1,6 +1,6 @@
 import pytest
-from simplerqgis.initializations import get_qqis_path, set_qgsapplication_instance
-from qgis.core import (QgsApplication)
+from simplerqgis.initializations import *
+from qgis.core import QgsApplication
 
 
 def test_get_qgis_path():
@@ -10,5 +10,11 @@ def test_get_qgis_path():
 
 def test_set_qgsapplication_instance():
     app = type(set_qgsapplication_instance())
+    app_true = type(QgsApplication([], True))
+    assert app == app_true
+
+
+def test_initialise_qgis_resource():
+    app = type(initialise_qgis_resource())
     app_true = type(QgsApplication([], True))
     assert app == app_true
